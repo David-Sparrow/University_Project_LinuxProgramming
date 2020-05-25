1961178547163c04f621a50641a70bb9ecf125bc
Dawid Wróbel <wrobel.dawid95@gmail.com>
2020-05-25 18:51:43 +0200

    Dodanie czystego "provider'a".

---
+++ provider.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <signal.h>
+#include <time.h>
+
+/**
+ * NOTATKI:
+ *
+ *
+ */
+
+
+/**
+ * Zmienne globalne
+ */
+
+
+/**
+ * Deklaracje funkcji
+ */
+
+
+/**
+ * Funkcja main
+ */
+int main(int argc, char * argv[])
+{
+
+
+    return 0;
+}
+
+/**
+ * Definicje funkcji
+ */
